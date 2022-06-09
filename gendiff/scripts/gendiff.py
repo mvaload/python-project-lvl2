2e71349d5dc1da032b4893f0c695fd12d593c062
mvaload <mvaload@gmail.com>
2022-06-09 09:44:51 +0300

    args and options

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', metavar='first_file')
     parser.add_argument('second_file', metavar='second_file')
+    parser.add_argument('-f', '--format', dest='format', help='set format of output')
     args = parser.parse_args()
     print(args)
 
