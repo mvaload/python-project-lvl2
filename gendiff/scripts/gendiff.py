812a270e3a48631066e0dc6b2bd6a05df8ebb7eb
mvaload <mvaload@gmail.com>
2022-06-09 17:35:17 +0300

    fix linter error

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,10 +3,12 @@ from gendiff.differ import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', metavar='first_file')
     parser.add_argument('second_file', metavar='second_file')
-    parser.add_argument('-f', '--format', dest='format', help='set format of output')
+    parser.add_argument('-f', '--format', dest='format',
+                        help='set format of output')
     args = parser.parse_args()
     result = generate_diff(args.first_file, args.second_file)
     print(result)
