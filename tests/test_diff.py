ee1e5c07def0ecb823faddee3cf5a80f467e4144
mvaload <mvaload@gmail.com>
2022-06-13 12:54:07 +0300

    added test for check yaml

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,18 +1,23 @@
 from gendiff.differ import generate_diff
 import os
-import pytest
 
 
-@pytest.fixture
-def path():
-    file_path1 = os.getcwd() + '/tests/fixtures/flat1.json'
-    file_path2 = os.getcwd() + '/tests/fixtures/flat2.json'
+def get_path(format):
+    file_path1 = os.getcwd() + f'/tests/fixtures/flat1.{format}'
+    file_path2 = os.getcwd() + f'/tests/fixtures/flat2.{format}'
     file_path_result = os.getcwd() + '/tests/fixtures/flat_result.txt'
     return file_path1, file_path2, file_path_result
 
 
-def test_diff_flat(path):
-    file_path1, file_path2, file_path_result = path
+def test_diff_flat_json():
+    file_path1, file_path2, file_path_result = get_path('json')
+    diff = generate_diff(file_path1, file_path2)
+    result = open(file_path_result).read()
+    assert diff == result
+
+
+def test_diff_flat_yaml():
+    file_path1, file_path2, file_path_result = get_path('yaml')
     diff = generate_diff(file_path1, file_path2)
     result = open(file_path_result).read()
     assert diff == result
