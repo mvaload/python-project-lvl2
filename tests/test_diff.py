1c7779af6477a9f0defa5984e00014ef2dadebcd
mvaload <mvaload@gmail.com>
2022-06-15 12:05:09 +0300

    added tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,23 +1,37 @@
-from gendiff.differ import generate_diff
+from difference_calculator.differ import generate_diff
 import os
 
 
-def get_path(format):
-    file_path1 = os.getcwd() + f'/tests/fixtures/flat1.{format}'
-    file_path2 = os.getcwd() + f'/tests/fixtures/flat2.{format}'
-    file_path_result = os.getcwd() + '/tests/fixtures/flat_result.txt'
-    return file_path1, file_path2, file_path_result
+def get_path(format, extension):
+    file_path1 = os.getcwd() + f'/tests/fixtures/file1.{extension}'
+    file_path2 = os.getcwd() + f'/tests/fixtures/file2.{extension}'
+    result_format = os.getcwd() + f'/tests/fixtures/result_{format}'
+    return file_path1, file_path2, result_format
 
 
-def test_diff_flat_json():
-    file_path1, file_path2, file_path_result = get_path('json')
-    diff = generate_diff(file_path1, file_path2)
-    result = open(file_path_result).read()
+def test_diff_stylish_json():
+    file_path1, file_path2, result_format = get_path('stylish', 'json')
+    diff = generate_diff(file_path1, file_path2, 'stylish')
+    result = open(result_format).read()
     assert diff == result
 
 
-def test_diff_flat_yaml():
-    file_path1, file_path2, file_path_result = get_path('yaml')
-    diff = generate_diff(file_path1, file_path2)
-    result = open(file_path_result).read()
+def test_diff_stylish_yaml():
+    file_path1, file_path2, result_format = get_path('stylish', 'yaml')
+    diff = generate_diff(file_path1, file_path2, 'stylish')
+    result = open(result_format).read()
+    assert diff == result
+
+
+def test_diff_plain_json():
+    file_path1, file_path2, result_format = get_path('plain', 'json')
+    diff = generate_diff(file_path1, file_path2, 'plain')
+    result = open(result_format).read()
+    assert diff == result
+
+
+def test_diff_plain_yaml():
+    file_path1, file_path2, result_format = get_path('plain', 'yaml')
+    diff = generate_diff(file_path1, file_path2, 'plain')
+    result = open(result_format).read()
     assert diff == result
